7fc0eba9eaa993bb3d99621af3d5d64b21dd41ac
wayson14 <wayson.1104@gmail.com>
2022-04-13 16:46:56 +0200

    Added debug_toolbar, added various settings files.

---
+++ vigo_guests/vigo_guests/settings/development.py
@@ -0,0 +1,20 @@
+from .base import *
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': '',
+}
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
