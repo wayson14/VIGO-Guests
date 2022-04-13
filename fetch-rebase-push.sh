c27d317995d3c1b35be990e5d6acefbee7297692
wayson14 <wayson.1104@gmail.com>
2022-04-13 22:39:39 +0200

    Added static collecting configuration

---
+++ fetch-rebase-push.sh
@@ -0,0 +1,3 @@
+git fetch origin main
+git rebase origin/main
+git push -u origin main
--- vigo_guests/settings/production.py
+++ vigo_guests/settings/production.py
@@ -2,3 +2,4 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['192.168.11.24']
+STATIC_ROOT = BASE_DIR / '..' / "static"
