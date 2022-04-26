755cebcd88138e10735ae76c9e272898a8830056
wayson14 <wayson.1104@gmail.com>
2022-04-26 12:04:05 +0200

    Minor hotfix typo

--- django-scripts/prod-full-start.sh
+++ django-scripts/prod-full-start.sh
@@ -13,6 +13,6 @@ export AUTH_LDAP_BIND_PASSWORD="!Wsparcie5005"
 pip install -r req.txt
 python ./manage.py makemigrations --settings=vigo_guests.settings.production
 python ./manage.py migrate --settings=vigo_guests.settings.production
-echo "yes" | python ./manage.py collectstatic --settings=vigo_guests.settings.productio
+echo "yes" | python ./manage.py collectstatic --settings=vigo_guests.settings.production
 
 daphne -b 0.0.0.0 -p 8000 --access-log daphne.log vigo_guests.asgi:application 
