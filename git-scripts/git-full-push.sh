252d7b271f532a4b90fdd356fa4220de434c7b1e
wayson14 <wayson.1104@gmail.com>
2022-04-26 12:01:36 +0200

    Bunch of new scripts

--- git-scripts/git-full-push.sh
+++ git-scripts/git-full-push.sh
@@ -1,4 +1,5 @@
-source ./git-scripts/add-commit.sh $1
-source 
+source ./git-scripts/add-commit.sh "$1"
+source ./venv/bin/activate
+pip freeze > req.txt
 source ./git-scripts/fetch-rebase-push.sh 
 source ./git-scripts/tofork-fetch-rebase-push.sh
