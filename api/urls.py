540c77b4db03d8e8c227cfc18dfb6dc6184f91c6
Rys00 <mateusz.ogniewski@gmail.com>
2022-04-29 10:46:49 +0200

    removed url path to importer to avoid accidential imports

--- api/urls.py
+++ api/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('no_cards_guests', NoCardsGuests.as_view()),
     path('free_cards', FreeCards.as_view()),
     path('active_guest_entries', ActiveGuestEntries.as_view()),
-    path('import', Importer.as_view())
+    #path('import', Importer.as_view())
 ]
