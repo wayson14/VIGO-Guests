2a1e956e1480ee8635cf9586d51d91ba586ad426
wayson14 <wayson.1104@gmail.com>
2022-04-29 13:13:46 +0200

    Added friendly date

--- api/serializers.py
+++ api/serializers.py
@@ -10,6 +10,16 @@ class CardSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class GuestEntrySerializer(serializers.ModelSerializer):
+    def parse_datetime(self, datetime_to_parse):
+        return datetime_to_parse.strftime("%Y-%m-%d %H:%M")
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        if representation['enter_datetime']:
+            representation['enter_datetime'] = self.parse_datetime(instance.enter_datetime)
+        if representation['exit_datetime']:
+            representation['exit_datetime'] = self.parse_datetime(instance.exit_datetime)
+        return representation
+    
     class Meta:
         model = GuestEntry
         fields = '__all__'
@@ -22,6 +32,7 @@ class GuestEntrySerializer(serializers.ModelSerializer):
     # validators = [
     #     card_is_not_given(data['card'])
     # ]
+
 class CardGivingSerializer(serializers.ModelSerializer):
     class Meta:
         model = GuestEntry
